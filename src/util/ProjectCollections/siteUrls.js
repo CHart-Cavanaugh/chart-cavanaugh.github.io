4505b73ddb11f51765fe7ff6f49a62463bedba45
Christopher Hart-Cavanaugh <christopher.hartcava@gmail.com>
2024-09-07 16:03:05 -0700

    Disable "MarkdownPreviewer" project.

--- src/util/ProjectCollections/siteUrls.js
+++ src/util/ProjectCollections/siteUrls.js
@@ -2,7 +2,7 @@ export const siteUrls = [
 
   'https://chart-cavanaugh.github.io/JavaScriptCalculator/',
   'https://chart-cavanaugh.github.io/RandomQuoteMachine/',
-  'https://chart-cavanaugh.github.io/MarkdownPreviewer/',
+  // 'https://chart-cavanaugh.github.io/MarkdownPreviewer/',
   'https://chart-cavanaugh.github.io/DrumMachine/',
   'https://chart-cavanaugh.github.io/AlanTuring_Tribute/',
 
