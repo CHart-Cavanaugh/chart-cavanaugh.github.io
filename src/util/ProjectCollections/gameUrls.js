5c48a1f900f27f357f40e4065408a5dabeb039ec
Christopher Hart-Cavanaugh <christopher.hartcava@gmail.com>
2024-09-29 02:13:29 -0700

    Update #1: Add new game project.
    Update #2: Reorganize game projects in order of
    descending quality.

--- src/util/ProjectCollections/gameUrls.js
+++ src/util/ProjectCollections/gameUrls.js
@@ -1,8 +1,9 @@
 export const gameUrls = [
 
-  'https://chart-cavanaugh.itch.io/test-upload',
+  'https://chart-cavanaugh.itch.io/storm-demo',
+  'https://chart-cavanaugh.itch.io/tankshowdown-demo',
   'https://chart-cavanaugh.itch.io/2dblockgrid-demo',
   'https://chart-cavanaugh.itch.io/wheelcatcher-demo',
-  'https://chart-cavanaugh.itch.io/tankshowdown-demo',
+  'https://chart-cavanaugh.itch.io/test-upload',
 
 ];
--- src/util/ProjectThumbnails/gameThumbnails.js
+++ src/util/ProjectThumbnails/gameThumbnails.js
@@ -2,6 +2,7 @@ import projectThumbnail1 from './../../images/gameProject1.png';
 import projectThumbnail2 from './../../images/gameProject2.png';
 import projectThumbnail3 from './../../images/gameProject3.png';
 import projectThumbnail4 from './../../images/gameProject4.png';
+import projectThumbnail5 from './../../images/gameProject5.png';
 
 
 
@@ -11,5 +12,6 @@ export const gameThumbnails = [
   projectThumbnail2,
   projectThumbnail3,
   projectThumbnail4,
+  projectThumbnail5,
 
 ];
